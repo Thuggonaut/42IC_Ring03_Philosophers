9f76de3d92bf9ec10766762f04ebae509b5bdbd8
Thuy Quematon <thuytkq@outlook.com>
2024-05-03 00:04:12 +0930

    u

--- whiteboard/wb.c
+++ whiteboard/wb.c
@@ -1,63 +1,84 @@
-# include <stdio.h> //To call printf() for printing to the STDOUT
-# include <stdlib.h> //To call malloc(), free() for memory management
-# include <stdint.h> 
-# include <sys/wait.h>
-# include <unistd.h> //To call write(), usleep()
-# include <stdbool.h> //To use boolean values
-# include <errno.h> //To access various error codes during execution
-# include <string.h> //To call memset()
-# include <pthread.h> //For thread related functions
-# include <sys/time.h> //To call gettimeofday()
-# include <limits.h> //To use the MIN and MAX macros
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<unistd.h>
 
+sem_t			forkk[5]; //5 to include a variable for each fork
+pthread_mutex_t mutex; //Mutex for critical sections
 
-//ANSI Escape Sequences for text formatting*****
-# define RESET		"\033[0m" //Reset to default color 
-# define RED		"\033[1;31m"
-# define GREEN		"\033[1;32m"
-# define YELLOW		"\033[1;33m"
-# define MAGENTA	"\033[1;35m"
-# define CYAN		"\033[1;36m"
+void	*philos(void *n); //DEFINE
+void	eat(int); //DEFINE
 
-void	error_exit(const char *error_msg)
+int		main(void)
 {
-	printf(MAGENTA "%s\n" RESET, error_msg);
-	exit(EXIT_FAILURE);
+	int			i = 0; //To keep track of the thread number
+	int			n[5];
+	pthread_t	p_t[5]; //One variable for each thread for each philosopher
+
+	//Initialize every semaphore variable
+	while (i < 5)
+	{
+		sem_init(&forkk[i], 0, 1); //Each fork is initially free
+		i++;
+	}
+
+	//Initialize mutex
+	pthread_mutex_init(&mutex, NULL); 
+
+	//Create a thread for each philosopher
+	i = 0;
+	while (i < 5)
+	{
+		n[i] = i;
+		pthread_create(&p_t[i], NULL, philos, (void *)&n[i]); //The `void` will be the input for the philos() ft
+		i++;
+	}
+
+	//Join each of the threads
+	i = 0;
+	while (i < 5)
+	{
+		pthread_join(p_t[i], NULL);
+		i++;
+	}
+
+	//Destroy mutex
+	pthread_mutex_destroy(&mutex); 
+
+	return (0);
 }
 
-//Define a function to convert the string input arguments into long integers. Static because it's only used for parsing here
-static long	ft_atol(const char *s)
+void	*philos(void *n)
 {
-	long		res; //To store the converted string to integer. Long to handle larger than int data
-	int			len; //To check the string length is not > 10, as INT_MAX has 10 digits
-	
-	res = 0;
-	while ((*s == 32 || *s >= 9 && *s <= 13)) //Skip leading whitespaces
-		s++;
-	if (*s == '-' || *s == '+') //Check for `-` signs, and skip `+` signs
-	{
-		if (*s == '-')
-			error_exit("Positive values only");
-		s++;
-	}
-	if (!(*s >= '0' && *s <= '9')) //Check for invalid characters that are not digits
-		error_exit("Digit values only");
-	len = 0;
-	while (*s >= '0' && *s <= '9')
-	{
-		res = (res * 10) + (*s - '0');
-		s++;
-		len++;
-	}
-	if (len > 10 || res > INT_MAX) //Check if the input is 10 or more digits, and if the result is > INT_MAX
-		error_exit("The value cannot exceed the INT_MAX value 2147483647");
-	return (res);
+	int		ph = *(int *)n; //`n` value will be `i`
+		
+	//Ensure that philosophers wait their turn to pick up forks
+	pthread_mutex_lock(&mutex);
+
+	printf("Philosopher %d is thinking\n", ph);
+	sem_wait(&forkk[ph]); //Ph will try to pick up the left fork, and if successful, continue
+
+	printf("Philosopher %d has taken a fork\n", ph);
+	//printf("Philosopher %d tries to pick up right fork\n", ph);
+	sem_wait(&forkk[(ph + 1) % 5]); //Ph will try to pick up the right fork, and if successful, continue
+
+	//printf("Philosopher %d picks up right fork\n", ph);
+	pthread_mutex_unlock(&mutex); //Unlock critical section
+
+	eat(ph); //Ph can eat now that left and right forks have been gathered
+	sleep(2); //The current thread sleeps, to allow the other threads to execute at the same time
+
+	sem_post(&forkk[(ph + 1) % 5]); //Let the next philosopher know the current philosopher is putting down the right fork
+	sem_post(&forkk[ph]); //Then puts down left fork
+
+	printf("Philosopher %d is sleeping\n", ph);
+	//printf("Philosopher %d puts down left fork\n", ph);
+
+	return (NULL);
 }
 
-
-int main() {
-    const char *test_string = "1234567890";
-    long result = ft_atol(test_string);
-    printf("Result: %ld\n", result);
-    return 0;
+void	eat(int ph)
+{
+	printf("Philosopher %d is eating\n", ph);
 }
